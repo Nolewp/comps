79e7d04f682bafc5e9b98e9546a3c7f2fd5bfde6
Ochs <jochs4fun@gmail.com>
2022-08-04 19:16:45 -0400

    add run watch

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,5 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
-    watch: true,
-    watchOptions: {
-        aggregateTimeout: 200,
-        poll: 1000,
-        ignored: '**/node_modules',
-      },
   },
 };
