df0e6bc91a8cb2fd974164e66b18b55e4202d2d4
Ochs <jochs4fun@gmail.com>
2022-08-04 23:07:18 -0400

    formated input div element correctly

--- src/calculator.js
+++ src/calculator.js
@@ -2,10 +2,10 @@ let wrapper = (addbtn, calcbtn) => {
     let hold = document.createElement('div');
     hold.classList.add("calcutor");
     hold.innerHTML = `
-    <div id = "calc"
-    <div><input type="number" name="sqft" id="sqft" placeholder="sq ft"> <input type="number" name="price" id="price" placeholder="price"> </div>
-    <div><input type="number" name="sqft" id="sqft" placeholder="sq ft"> <input type="number" name="price" id="price" placeholder="price"> </div>
-    <div><input type="number" name="sqft" id="sqft" placeholder="sq ft"> <input type="number" name="price" id="price" placeholder="price"> </div>
+    <div id = "calc">
+        <div><input type="number" name="sqft" id="sqft" placeholder="sq ft"> <input type="number" name="price" id="price" placeholder="price"> </div>
+        <div><input type="number" name="sqft" id="sqft" placeholder="sq ft"> <input type="number" name="price" id="price" placeholder="price"> </div>
+        <div><input type="number" name="sqft" id="sqft" placeholder="sq ft"> <input type="number" name="price" id="price" placeholder="price"> </div>
     </div> 
     ` 
     hold.appendChild(addbtn)
@@ -23,6 +23,13 @@ let addButton = () => {
     return addcalcbutton
 }
 
+let addInput = () => {
+    let div = document.createElement('div');
+    div.classList.add('h');
+    div.innerHTML = `<input type="number" name="sqft" id="sqft" placeholder="sq ft"> <input type="number" name="price" id="price" placeholder="price">` ;
+    document.getElementById('calc').appendChild(div)
+}
+
 let calculateBtn = () => {
     let calculateButton = document.createElement('button');
     calculateButton.classList.add("addHomeBtn");
@@ -32,12 +39,6 @@ let calculateBtn = () => {
     }
     return calculateButton
 }
-let addInput = () => {
-    let div = document.createElement('div');
-    div.classList.add('h');
-    div.innerHTML = `<input type="number" name="sqft" id="sqft" placeholder="sq ft"> <input type="number" name="price" id="price" placeholder="price">` ;
-    document.getElementById('calc').appendChild(div)
-}
 
 let getInput = () => {
     let sqftValue = document.querySelectorAll('#sqft');
@@ -46,13 +47,19 @@ let getInput = () => {
     let totalPrice = 0 
     sqftValue.forEach(num => sqft += parseFloat(num.value))
     priceValue.forEach(num => totalPrice += parseFloat(num.value))
-    displayComp(sqft, totalPrice)
+    displayComp(totalPrice, sqft)
 }
 
 let displayComp = (val1, val2) => {
     let comp = val1/val2
-    console.log(parseInt(comp))
-    return comp
+    upddateComp(comp)
+}
+
+let upddateComp = (newcomp) => {
+    let compValue = document.createElement('h4');
+    compValue.classList.add('compValue');
+    compValue.innerText = newcomp;
+    document.body.appendChild(compValue)
 }
 
 let createCalculator = () => {
