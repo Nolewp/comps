5aa1217ed4c639f54c4d287c621be5c4fb60476f
Jack Ochs <jochs4fun@gmail.com>
2024-04-22 18:12:00 -0400

    Added tailwind to project

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    watchOptions: {
+        aggregateTimeout: 6000,
+        poll: 10000,
+        ignored: '**/node_modules',
     },
     module: {
         rules: [
